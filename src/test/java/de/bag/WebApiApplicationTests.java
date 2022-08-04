5fd82ac85826dcc1b5effa465cc36d5bfa30099c
Timo Krischik <23027596+MrMutantus@users.noreply.github.com>
2022-08-04 16:02:04 +0200

    Initial commit

---
+++ src/test/java/de/bag/WebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
